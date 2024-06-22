7ccf33c74d3e6fdb0b3b7a73619112769afc9707
baxt <habibulaevhushbaht05@gmail.com>
2024-06-22 12:49:26 +0600

    chanegd setup

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ def readme():
 
 
 setup(
-  name='-',
+  name='django-rest-framework-reset-password',
   version='1.0.0',
   author='xwbxtdev',
   author_email='example@gmail.com',
   description='This module for auth regiters in django',
   long_description=readme(),
   long_description_content_type='text/markdown',
-  url='https://github.com/baxtdev/',
+  url='https://github.com/baxtdev/django_rest_framework_reset_password',
   packages=find_packages(),
   install_requires=['django','rest_framework','markdown','django-rest-registration'],
   classifiers=[
